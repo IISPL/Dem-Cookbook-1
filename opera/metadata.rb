5dc9065bf27d83643734d534d572a6be180c869f
IISPL <pallavij@interfaceinfosoft.com>
2015-09-15 17:45:22 +0530

    final-qsa

---
+++ opera/metadata.rb
@@ -0,0 +1,13 @@
+name 'opera'
+maintainer 'Dennis Hoer'
+maintainer_email 'dennis.hoer@gmail.com'
+license 'MIT'
+description 'Installs Opera browser'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '1.0.0'
+
+# supports 'mac_os_x'
+supports 'ubuntu'
+supports 'windows'
+
+suggests 'apt', '~> 3.2'
