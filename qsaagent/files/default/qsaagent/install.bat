d6ddbb478f3d1e8f5ac3becfb3b4ce364035ee68
IISPL <pallavij@interfaceinfosoft.com>
2015-09-15 14:51:26 +0530

    Revert "QSA"
    This reverts commit 707d42dad622a20fdb837c166c8da8ac9628bffb.

--- qsaagent/files/default/qsaagent/install.bat
+++
@@ -1,4 +0,0 @@
-
-@echo off
-echo Installing JRE...
-start /w C:\qsaagent\Java\jre-8u51-windows-x64.exe /s INSTALLDIR=C:\java\jre8 REBOOT=Suppress
--- qsaagent/files/default/qsaagent/run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-SET path="C:\java\jre8\bin";%path%
-SET BASE_DIRECTORY=%~1
-IF DEFINED BASE_DIRECTORY (@set COMMAND=%BASE_DIRECTORY%\QSAgent-Desktop-1.0.0.jar %BASE_DIRECTORY%) ELSE (@set COMMAND=QSAgent-Desktop-1.0.0.jar)
-
-start "QSAgent" javaw -jar %COMMAND%
-exit
