0c0c218293ee5e0be964a4c1b4632496bf55cbf2
IISPL <pallavij@interfaceinfosoft.com>
2015-09-15 11:31:11 +0530

    QAS2

---
+++ qsaagent2/files/default/qsaagent/install.bat
@@ -0,0 +1,4 @@
+
+@echo off
+echo Installing JRE...
+start /w C:jre-8u51-windows-x64.exe /s INSTALLDIR=C:\QSA-Agent\Java\jre1.8.0_51 REBOOT=Suppress
---
+++ qsaagent2/metadata.rb
@@ -0,0 +1,10 @@
+name             'qsaagent2'
+maintainer       'IISPL'
+maintainer_email 'prafult@interfaceinfosof.com'
+license          'All rights reserved'
+description      'Installs/Configures qsaagent2'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends "windows"
+provides "remote_directory"
