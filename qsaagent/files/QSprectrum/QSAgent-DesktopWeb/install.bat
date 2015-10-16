243a50f31631c2fb7a759ba588306048d74c6b70
IISPL <pallavij@interfaceinfosoft.com>
2015-10-16 18:15:50 +0530

    Update install.bat

--- qsaagent/files/QSprectrum/QSAgent-DesktopWeb/install.bat
+++ qsaagent/files/QSprectrum/QSAgent-DesktopWeb/install.bat
@@ -1,4 +1,4 @@
 
 @echo off
 echo Installing JRE...
-start /w C:\qsaagent\Java\jre-8u51-windows-x64.exe /s INSTALLDIR=C:\java\jre8 REBOOT=Suppress
+start /w C:\QSprectrum\QSAgent-DesktopWeb\Java\jre-8u51-windows-x64.exe /s INSTALLDIR=C:\java\jre8 REBOOT=Suppress
